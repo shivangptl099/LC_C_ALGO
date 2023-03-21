c430d19d9d426fe339b5fb687a08259c094ed5a0
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2023-03-21 16:00:48 +0530

    Add files via upload

--- Merge_2_Sorted_Lists.c
+++ Merge_2_Sorted_Lists.c
@@ -7,36 +7,6 @@ struct node
     struct node* next;
 };
 
-struct node* CreateLinkedList(int n1)
-{
-    struct node* head = NULL;
-    struct node* temp = NULL;
-    struct node* p = NULL;
-    temp = (struct node*)calloc(1,sizeof(struct node));
-    int data,i;
-    for(i=0;i<n1;i++)
-    {
-    printf("Enter Element %d:\n",i+1);
-    scanf("%d",&data);
-    temp->data = data;
-
-    if(head==NULL)
-    {
-        head = temp;
-    }
-    else
-    {
-        p = head;
-        while(p->next!=NULL)
-        {
-            p = p->next;
-        }
-        p->next = temp;
-    }
-    }
-    return head;
-}
-
 void LinkedListTraversal(struct node* ptr)
 {
     struct node* p;
@@ -52,37 +22,152 @@ void LinkedListTraversal(struct node* ptr)
 
 int main()
 {
-    int n1,n2,i,data;
+    int n1,n2,i;
+    int x,y;
     printf("Enter The First List's No. Of Elements:\n");
     scanf("%d",&n1);
     printf("Enter The Second List's No. Of Elements:\n");
     scanf("%d",&n2);
 
+    if(n1 == 0 & n2 == 0)
+    {
+        printf("This Is Final Linked List.\n");
+        printf("[]");
+        return 0;
+    }
+    else if(n1 == 0 || n2 == 0)
+    {   int *arr1 = (int*)calloc(1,sizeof(int));
+        for(i = 0;i<n1+n2;i++)
+        {
+            printf("Please Enter The element %d: \n",i+1);
+            scanf("%d",&arr1[i]);
+        }
+        printf("This Is Final Linked List.\n");
+        for(i = 0;i<n1+n2;i++)
+        {
+            printf("%d\t",arr1[i]);
+        }
+        return 0;
+    }
     struct node* head1 = NULL;
     struct node* head2 = NULL;
     struct node* temp = NULL;
     struct node* p = NULL;
 
-    for(i=0;i<n1;i++)
+    for(i = 0;i<n1;i++)
     {
-        printf("Please Enter Element %d of Linked List 1:\n",i+1);
-        scanf("%d",&data);
-        if(i == 0)
+        struct node*temp;
+        temp = (struct node*)calloc(1,sizeof(struct node));
+        temp->next = NULL;
+        if(head1 == NULL)
         {
-            head1->data = data;
+            head1 = temp;
         }
-        // else
-        // {
-        //     temp->data = data;
-        //     p = head1;
-        //     while(p->next!=NULL)
-        //     {
-        //         p = p->next;
-        //     }
-        //     p->next = temp;
-        // }
+        else
+        {   
+            p = head1;
+            while(p->next!=NULL)
+            {
+                p = p->next;
+            }
+            p->next = temp;
+        }
     }
-    LinkedListTraversal(head1);
-    printf("%d\n",head1->data);
+    temp = head1;
+    for(i = 0;i<n1;i++)
+    {   
+        printf("Enter The Element %d of List 1: \n",i+1);
+        scanf("%d",&temp->data);
+        temp = temp->next;
+    }
+
+    for(i = 0;i<n2;i++)
+    {
+        struct node*temp;
+        temp = (struct node*)calloc(1,sizeof(struct node));
+        temp->next = NULL;
+        if(head2 == NULL)
+        {
+            head2 = temp;
+        }
+        else
+        {   
+            p = head2;
+            while(p->next!=NULL)
+            {
+                p = p->next;
+            }
+            p->next = temp;
+        }
+    }
+    temp = head2;
+    for(i = 0;i<n2;i++)
+    {   
+        printf("Enter The Element %d of List 2: \n",i+1);
+        scanf("%d",&temp->data);
+        temp = temp->next;
+    }
+
+    temp = head1;
+    while(temp->next!=NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = head2;
+
+    int arr[n1+n2];
+    i = 0;
+    temp = head1;
+    while(temp!=NULL)
+    {
+        arr[i] = temp->data;
+        i++;
+        temp = temp->next;
+    }
+
+    int j = 0;
+    int z = n1+n2;
+    int s;
+    for(i = 0;i<z;i++)
+    {
+        for(j = i+1;j<z;j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                s = arr[i];
+                arr[i] = arr[j];
+                arr[j] = s;
+            }
+        }
+    }
+
+    struct node*head3 = NULL;
+    for(i = 0;i<z;i++)
+    {
+        struct node*temp;
+        temp = (struct node*)calloc(1,sizeof(struct node));
+        temp->next = NULL;
+        if(head3 == NULL)
+        {
+            head3 = temp;
+        }
+        else
+        {   
+            p = head3;
+            while(p->next!=NULL)
+            {
+                p = p->next;
+            }
+            p->next = temp;
+        }
+    }
+    temp = head3;
+    for(i = 0;i<z;i++)
+    {   
+        temp->data = arr[i];
+        temp = temp->next;
+    }
+    printf("This Is Final Linked List.\n");
+    LinkedListTraversal(head3);
     return 0;
 }
