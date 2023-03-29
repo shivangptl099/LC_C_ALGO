a900a1d935ec7409b321be60df363be902b781c1
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2023-03-29 19:54:29 +0530

    Update Single_Number.c

--- Single_Number.c
+++ Single_Number.c
@@ -1,33 +1,42 @@
-int singleNumber(int* nums, int numsSize){
+#include<stdio.h>
+#include<malloc.h>
+int SingleNumber(int* nums, int numsSize){
     int i,j,flag;
     for(i = 0;i<numsSize-1;i++)
     {
-        for(int j = i+1;j<numsSize;j++)
+        for(int j = 0;j<numsSize;j++)
         {   flag = 1;
+            if(i == j)
+            {
+                continue;
+            }
             if(nums[i] == nums[j])
             {
                 flag++;
+                break;
             }
         }
-        if(flag != 2)
+        if(flag%2 != 0)
         {
             break;
         }
     }
     int z = nums[i];
-    i = numsSize-1;
-    flag = 1;
-    for(j = 0;j<numsSize-1;j++)
-    {  
-        if(nums[i] == nums[j])
-        {
-            flag++;
-        }
-    }
-    if(flag!=2)
-    {
-        return nums[numsSize-1];
-    }
-
     return z;
+}
+
+int main()
+{   
+    int i,n;
+    printf("How Many Elements Are There In Your Array:\n");
+    scanf("%d",&n);
+    int* arr = (int*)malloc(n*sizeof(int));
+    for(i = 0;i<n;i++)
+    {
+        printf("Enter element %d: \n",i+1);
+        scanf("%d",&arr[i]);
+    }
+    int x = SingleNumber(arr,n);
+    printf("The Element Present Only One Time Is : %d.\n",x);
+    return 0;
 }
