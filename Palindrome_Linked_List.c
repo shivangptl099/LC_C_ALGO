41ad8f6534b5d5665328e1d18eeb1fb3e7d9ffec
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2023-04-07 11:06:21 +0530

    Update Palindrome_Linked_List.c

--- Palindrome_Linked_List.c
+++ Palindrome_Linked_List.c
@@ -34,10 +34,50 @@ int isPalindrome(struct node* head){
         }
         j--;
     }
-    return ;
+    return 1;
 }
 
 int main()
 {
+    int i,n;
+    printf("How Many Elements Are There In Your Linked List?\n");
+    scanf("%d",&n);
+    struct node* head = NULL;
+    for(i = 0;i<n;i++)
+    {
+        struct node* temp = (struct node*)malloc(sizeof(struct node));
+        temp->next = NULL;
+        if(head == NULL)
+        {
+            head = temp;
+        }
+        else
+        {
+            struct node* p = head;
+            while(p->next!=NULL)
+            {
+                p = p->next;
+            }
+            p->next = temp;
+        }
+    }
+    struct node* p = head;
+    i = 1;
+    while(p!=NULL)
+    {
+        printf("Enter Element %d:\n",i);
+        scanf("%d",&p->data);
+        p = p->next;
+        i++;
+    }
+    int s = isPalindrome(head);
+    if(s == 1)
+    {
+        printf("The Linked List Is Palindrome.\n");
+    }
+    else
+    {
+        printf("The Linked List Is Not Palindrome.\n");
+    }
     return 0;
 }
