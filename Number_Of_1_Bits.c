45b60116988f5f5753c6de8db12d1976e044bb71
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2023-04-11 23:32:00 +0530

    Update Number_Of_1_Bits.c

--- Number_Of_1_Bits.c
+++ Number_Of_1_Bits.c
@@ -1,29 +1,15 @@
-#include<stdio.h>
-#include<stdint.h>
-
 int hammingWeight(uint32_t n) {
-    int temp = n;
     int r;
     int x = 0;
-    while(temp>0)
+    while(n>0)
     {
         r = 0;
-        r = temp % 10;
+        r = n % 2;
         if(r == 1)
         {
             x++;
         }
-        temp = temp / 10;
+        n = n / 2;
     }
     return x;
-}
-
-int main()
-{
-    uint32_t n;
-    printf("Please Enter The Number:\n");
-    scanf("%d",&n);
-    int x = hammingWeight(n);
-    printf("Amswer: %d",x);
-    return 0;
 }
