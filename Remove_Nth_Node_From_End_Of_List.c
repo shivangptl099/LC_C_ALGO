a5f78200be1e90e2b1e12b61518a405d5fa5efe5
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2023-04-24 11:25:39 +0530

    Update Remove_Nth_Node_From_End_Of_List.c

--- Remove_Nth_Node_From_End_Of_List.c
+++ Remove_Nth_Node_From_End_Of_List.c
@@ -1,6 +1,10 @@
-struct ListNode{
-    int val;
+#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode
+{
     struct ListNode* next;
+    int val;
 };
 
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
@@ -11,7 +15,7 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
         p = p->next;
         s++;
     }
-    int x = (s-n);
+    int x = (s-n-1);
     int i = 0;
     p = head;
     while(i<x)
@@ -22,4 +26,41 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     struct ListNode* q = p->next;
     p->next = q->next;
     return head;
+}
+
+int main()
+{
+    struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* a = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* b = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* p = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* q = (struct ListNode*)malloc(sizeof(struct ListNode));
+
+    int n = 2;
+    head->val = 1;
+    a->val = 2;
+    b->val = 3;
+    p->val = 4;
+    q->val = 5;
+
+    head->next = a;
+    a->next = b;
+    b->next = p;
+    p->next = q;
+    q->next = NULL; 
+
+    struct ListNode* temp = head;
+    while(temp!=NULL)
+    {
+        printf("%d\t",temp->val);
+        temp = temp->next;
+    }
+    printf("\n");
+    temp = removeNthFromEnd(head,n);
+    while(temp!=NULL)
+    {
+        printf("%d\t",temp->val);
+        temp = temp->next;
+    }
+    return 0;
 }
