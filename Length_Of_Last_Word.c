0b84727df4cdff92f694e6f25c84998c61553d37
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2023-08-20 21:39:39 +0530

    Update Length_Of_Last_Word.c

--- Length_Of_Last_Word.c
+++ Length_Of_Last_Word.c
@@ -1,43 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-int main()
-{
-    char* arr = (char*)calloc(1,sizeof(char));
-    printf("Enter the Sentence:\n");
-    scanf("%[^\n]%*c",arr);
-    int CurrLen = strlen(arr) - 1; 
-    int count = 0;
-    if(strlen(arr) == 0)
+int lengthOfLastWord(char * s){
+    int i,count;
+    count = 0;
+    for(i = strlen(s)-1;i>=0;i--)
     {
-        printf("Answer = 0");
-        return 0;
-    }
-    int i = CurrLen;
-    while(i>=0)
-    {
-        if(arr[i]!=' ')
+        if(s[i]!=' ')
         {
-            break;
-        }
-        else
-        {
-            i--;
-        }
-    }
-    char* arr1 = (char*)calloc(1,sizeof(char));
-    for(i;i>=0;i--)
-    {
-        if(arr[i] == ' ')
-        {
-            break;
-        }
-        else
-        {   
             count++;
         }
+        if(count>0 && s[i]==' ')
+        {
+            break;
+        }
     }
-    printf("%d",count);
-    return 0;
+    return count;
 }
